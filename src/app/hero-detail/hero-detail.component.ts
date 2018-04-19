f83b01f876e6042daca9234c6f0e3f8e9e7c5226
Ashish Karki <akarki@mimecast.com>
2018-04-19 09:14:56 -0400

    My own logic to populate hero initially in hero details. It's throwing heroForm initialization error though.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -2,45 +2,70 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { states, Address, Hero, heroes } from '../data-model';
+import { HeroService } from '../hero.service';
 
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+	selector: 'app-hero-detail',
+	templateUrl: './hero-detail.component.html',
+	styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnChanges {
-  @Input() hero: Hero;
+	@Input() hero: Hero;
 
-  heroForm: FormGroup;
-  states = states;
+	heroForm: FormGroup;
+	states = states;
 
-  constructor(private fb: FormBuilder) {
-    this.createForm();
-  }
+	constructor(private fb: FormBuilder, private heroService: HeroService) {
+		this.createForm();
+	}
 
-  createForm() {
-    this.heroForm = this.fb.group({
-      name: ['', Validators.required ],
-      address: this.fb.group({
-        street: '',
-        city: '',
-        state: '',
-        zip: ''
-      }),
-      power: '',
-      sidekick: ''
-    });
-  }
+	// ngOnInit() {
+
+	// 	this.heroService.getHeroes().subscribe(heroes => {
+	// 		const selectedHero = heroes.find(storedHero => storedHero.name === this.hero.name);
+			
+	// 	});
+	// }
+
+	// createForm(selectedHero: Hero) {
+	// 	this.heroForm = this.fb.group({
+	// 		name: [selectedHero.name, Validators.required ],
+	// 		address: this.fb.group({
+	// 			street: selectedHero.addresses[0].street,
+	// 			city: selectedHero.addresses[0].city,
+	// 			state: selectedHero.addresses[0].state,
+	// 			zip: selectedHero.addresses[0].zip
+	// 		}),
+	// 		power: '',
+	// 		sidekick: ''
+	// 	});
+	// }
+
+	createForm() {
+		this.heroForm = this.fb.group({
+			name: ['', Validators.required ],
+			address: this.fb.group({
+				street: '',
+				city: '',
+				state: '',
+				zip: ''
+			}),
+			power: '',
+			sidekick: ''
+		});
+	}
 
   ngOnChanges() { // <-- call rebuildForm in ngOnChanges
-    this.rebuildForm();
+  	if(this.heroForm !== undefined) {
+  		this.rebuildForm();
+  	}
   }
 
   rebuildForm() { // <-- wrap patchValue in rebuildForm
-    this.heroForm.reset();
-    this.heroForm.patchValue({
-      name: this.hero.name
-    });
+  	this.heroForm.reset();
+  	this.heroForm.patchValue({
+  		name: this.hero.name
+  	});
   }
 }
